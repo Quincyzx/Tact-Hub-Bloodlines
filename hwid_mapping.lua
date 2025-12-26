c5984dd7a04f96ebebaf536b0c824519dc562f5d
Quincyzx <tactsolos123@gmail.com>
2025-12-26 00:18:18 -0500

    Update hwid_mapping.lua

--- hwid_mapping.lua
+++ hwid_mapping.lua
@@ -1,9 +1,8 @@
 -- HWID to Key Mappings
--- Format: [HWID] = "key"
--- This file is automatically updated when users authenticate
+
 
 return {
-    -- Example: ["HWID-HERE"] = "tact-A7F3-K9Q2-8MZ4",
+    
 }
 
 
